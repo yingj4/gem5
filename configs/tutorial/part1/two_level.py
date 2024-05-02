60da30c6ff1e55600cbcf92c180dc6e066b5c86d
yingj4 <yingj4@illinois.edu>
2024-05-01 21:37:10 -0500

    tutorial part 1 caches

--- configs/tutorial/part1/two_level.py
+++ configs/tutorial/part1/two_level.py
@@ -161,7 +161,7 @@ system.cpu.icache.connectBus(system.l2bus)
 system.cpu.dcache.connectBus(system.l2bus)
 
 # Create an L2 cache and connect it to the l2bus
-system.l2cache = L2Cache(args)
+system.l2cache = L2Cache()
 system.l2cache.connectCPUSideBus(system.l2bus)
 
 # Create a memory bus
