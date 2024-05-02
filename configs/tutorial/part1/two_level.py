91022841380e907e3ea7b1ec8c0e01d20106a659
yingj4 <yingj4@illinois.edu>
2024-05-01 21:36:01 -0500

    tutorial part 1 caches

--- configs/tutorial/part1/two_level.py
+++ configs/tutorial/part1/two_level.py
@@ -1,62 +1,206 @@
+# import m5
+# from m5.objects import *
+# from caches import *
+
+# s = System()
+
+# s.clk_domain = SrcClockDomain()
+# s.clk_domain.clock = '1GHz'
+# s.clk_domain.voltage_domain = VoltageDomain()
+
+# s.mem_mode = 'timing'
+# s.mem_ranges = [AddrRange('512MB')]
+
+# s.cpu = X86TimingSimpleCPU()
+
+# s.cpu.icache = L1ICache()
+# s.cpu.dcache = L1DCache()
+# s.cpu.icache.connectCPU(s.cpu)
+# s.cpu.dcache.connectCPU(s.cpu)
+
+
+# s.l2bus = L2XBar()
+# s.cpu.icache.connectBus(s.l2bus)
+# s.cpu.dcache.connectBus(s.l2bus)
+
+# s.l2cache = L2Cache()
+# s.l2cache.connectCPUSideBus(s.l2bus)
+# s.membus = SystemXBar()
+# s.l2cache.connectMemSideBus(s.membus)
+
+# s.cpu.createInterruptController()
+# s.cpu.interrupts[0].pio = s.membus.mem_side_ports
+# s.cpu.interrupts[0].int_requestor = s.membus.cpu_side_ports
+# s.cpu.interrupts[0].int_responder = s.membus.mem_side_ports
+
+# s.system_port = s.membus.cpu_side_ports
+
+# s.mem_ctrl = MemCtrl()
+# s.mem_ctrl.dram = DDR3_1600_8x8()
+# s.mem_ctrl.dram.range = s.mem_ranges[0]
+# s.mem_ctrl.port = s.membus.mem_side_ports
+
+# thispath = os.path.dirname(os.path.realpath(__file__))
+# binary = os.path.join(thispath, "../../../", "tests/test-progs/hello/bin/x86/linux/hello")
+# # binary = "/home/gem5/tests/test-progs/hello/bin/x86/linux/hello"
+
+# s.workload = SEWorkload.init_compatible(binary)
+
+# process = Process()
+# process.cmd = [binary]
+# s.cpu.workload = process
+# s.cpu.createThreads()
+
+
+# root = Root(full_system = False, system = s)
+# m5.instantiate()
+
+
+# print("Beginning simulation!")
+# exit_event = m5.simulate()
+# print('Exiting @ tick {} because {}'
+#       .format(m5.curTick(), exit_event.getCause()))
+
+# Copyright (c) 2015 Jason Power
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+""" This file creates a single CPU and a two-level cache system.
+This script takes a single parameter which specifies a binary to execute.
+If none is provided it executes 'hello' by default (mostly used for testing)
+
+See Part 1, Chapter 3: Adding cache to the configuration script in the
+learning_gem5 book for more information about this script.
+This file exports options for the L1 I/D and L2 cache sizes.
+
+IMPORTANT: If you modify this file, it's likely that the Learning gem5 book
+           also needs to be updated. For now, email Jason <power.jg@gmail.com>
+
+"""
+
+# import the m5 (gem5) library created when gem5 is built
 import m5
+
+# import all of the SimObjects
 from m5.objects import *
+
+# Add the common scripts to our path
+m5.util.addToPath("../../")
+
+# import the caches which we made
 from caches import *
 
-s = System()
+# import the SimpleOpts module
+from common import SimpleOpts
 
-s.clk_domain = SrcClockDomain()
-s.clk_domain.clock = '1GHz'
-s.clk_domain.voltage_domain = VoltageDomain()
-
-s.mem_mode = 'timing'
-s.mem_ranges = [AddrRange('512MB')]
-
-s.cpu = X86TimingSimpleCPU()
-
-s.cpu.icache = L1ICache()
-s.cpu.dcache = L1DCache()
-s.cpu.icache.connectCPU(s.cpu)
-s.cpu.dcache.connectCPU(s.cpu)
-
-
-s.l2bus = L2XBar()
-s.cpu.icache.connectBus(s.l2bus)
-s.cpu.dcache.connectBus(s.l2bus)
-
-s.l2cache = L2Cache()
-s.l2cache.connectCPUSideBus(s.l2bus)
-s.membus = SystemXBar()
-s.l2cache.connectMemSideBus(s.membus)
-
-s.cpu.createInterruptController()
-s.cpu.interrupts[0].pio = s.membus.mem_side_ports
-s.cpu.interrupts[0].int_requestor = s.membus.cpu_side_ports
-s.cpu.interrupts[0].int_responder = s.membus.mem_side_ports
-
-s.system_port = s.membus.cpu_side_ports
-
-s.mem_ctrl = MemCtrl()
-s.mem_ctrl.dram = DDR3_1600_8x8()
-s.mem_ctrl.dram.range = s.mem_ranges[0]
-s.mem_ctrl.port = s.membus.mem_side_ports
-
+# Default to running 'hello', use the compiled ISA to find the binary
+# grab the specific path to the binary
 thispath = os.path.dirname(os.path.realpath(__file__))
-binary = os.path.join(thispath, "../../../", "tests/test-progs/hello/bin/x86/linux/hello")
-# binary = "/home/gem5/tests/test-progs/hello/bin/x86/linux/hello"
+default_binary = os.path.join(
+    thispath,
+    "../../../",
+    "tests/test-progs/hello/bin/x86/linux/hello",
+)
 
-s.workload = SEWorkload.init_compatible(binary)
+# Binary to execute
+SimpleOpts.add_option("binary", nargs="?", default=default_binary)
 
+# Finalize the arguments and grab the args so we can pass it on to our objects
+args = SimpleOpts.parse_args()
+
+# create the system we are going to simulate
+system = System()
+
+# Set the clock frequency of the system (and all of its children)
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = "1GHz"
+system.clk_domain.voltage_domain = VoltageDomain()
+
+# Set up the system
+system.mem_mode = "timing"  # Use timing accesses
+system.mem_ranges = [AddrRange("512MB")]  # Create an address range
+
+# Create a simple CPU
+system.cpu = X86TimingSimpleCPU()
+
+# Create an L1 instruction and data cache
+system.cpu.icache = L1ICache(args)
+system.cpu.dcache = L1DCache(args)
+
+# Connect the instruction and data caches to the CPU
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
+
+# Create a memory bus, a coherent crossbar, in this case
+system.l2bus = L2XBar()
+
+# Hook the CPU ports up to the l2bus
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
+
+# Create an L2 cache and connect it to the l2bus
+system.l2cache = L2Cache(args)
+system.l2cache.connectCPUSideBus(system.l2bus)
+
+# Create a memory bus
+system.membus = SystemXBar()
+
+# Connect the L2 cache to the membus
+system.l2cache.connectMemSideBus(system.membus)
+
+# create the interrupt controller for the CPU
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+
+# Connect the system up to the membus
+system.system_port = system.membus.cpu_side_ports
+
+# Create a DDR3 memory controller
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
+
+system.workload = SEWorkload.init_compatible(args.binary)
+
+# Create a process for a simple "Hello World" application
 process = Process()
-process.cmd = [binary]
-s.cpu.workload = process
-s.cpu.createThreads()
+# Set the command
+# cmd is a list which begins with the executable (like argv)
+process.cmd = [args.binary]
+# Set the cpu to use the process as its workload and create thread contexts
+system.cpu.workload = process
+system.cpu.createThreads()
 
-
-root = Root(full_system = False, system = s)
+# set up the root SimObject and start the simulation
+root = Root(full_system=False, system=system)
+# instantiate all of the objects we've created above
 m5.instantiate()
 
-
-print("Beginning simulation!")
+print(f"Beginning simulation!")
 exit_event = m5.simulate()
-print('Exiting @ tick {} because {}'
-      .format(m5.curTick(), exit_event.getCause()))
+print(f"Exiting @ tick {m5.curTick()} because {exit_event.getCause()}")
