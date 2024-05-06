9d8b46d3b83389a0f03dc8c87c4798df57c9f3cc
yingj4 <yingj4@illinois.edu>
2024-05-06 14:35:48 -0500

    tutorial part 2 simobject

--- src/tutorial/part2/HelloObject.py
+++
@@ -1,7 +0,0 @@
-from m5.params import *
-from m5.SimObject import SimObject
-
-class HelloObject(SimObject):
-    type = 'HelloObject'
-    cxx_header = 'tutorial/part2/hello_object.hh'
-    cxx_class = 'gem5::HelloObject'
