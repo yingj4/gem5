8ea9d5ac224102e0ea0c6a99d0c18c8dd682a8df
yingj4 <yingj4@illinois.edu>
2024-05-06 21:25:27 -0500

    tutorial part 2 simobject

--- src/learning_gem5/part2/hello_object.cc
+++ src/learning_gem5/part2/hello_object.cc
@@ -1,13 +1,16 @@
-#include "learning_gem5/part2/hello_object.hh"
-
 #include <iostream>
+
+#include "base/trace.hh"
+#include "debug/HelloExample.hh"
+#include "learning_gem5/part2/hello_object.hh"
 
 namespace gem5
 {
 
 HelloObject::HelloObject(const HelloObjectParams& params) : SimObject(params)
 {
-    std::cout << "Hello World! From a SimObject!" << std::endl;
+    // std::cout << "Hello World! From a SimObject!" << std::endl;
+    DPRINTF(HelloExample, "Created the hello object\n");
 }
 
 } // namespace gem5
