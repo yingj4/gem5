92c6147d2e9bca4a6cc8e934cfd35072070ec5d7
yingj4 <yingj4@illinois.edu>
2024-05-08 22:37:46 -0500

    tutorial part 2 SimpleMemobj

--- src/learning_gem5/part2/simple_memobj.cc
+++ src/learning_gem5/part2/simple_memobj.cc
@@ -1,17 +1,181 @@
+// #include "learning_gem5/part2/simple_memobj.hh"
+
+// #include "debug/SimpleMemobj.hh"
+
+// namespace gem5
+// {
+
+// SimpleMemobj::SimpleMemobj(SimpleMemobjParams& params) : SimObject(params), instPort(params.name + ".inst_port", this), dataPort(params.name + ".data_port", this), memPort(params.name + ".mem_port", this), blocked(false)
+// {
+// }
+
+// Port& SimpleMemobj::getPort(const std::string& if_name, PortID idx)
+// {
+//     panic_if(idx != InvalidPortID, "This object does not support vector ports\n");
+
+//     // This is the name from the Python SimObject declaration (SimpleMemobj.py)
+//     if (if_name == "mem_side") {
+//         return memPort;
+//     } else if (if_name == "inst_port") {
+//         return instPort;
+//     } else if (if_name == "data_port") {
+//         return dataPort;
+//     } else {
+//         return SimObject::getPort(if_name, idx);
+//     }
+// }
+
+// AddrRangeList SimpleMemobj::CPUSidePort::getAddrRanges() const
+// {
+//     return owner->getAddrRanges();
+// }
+
+// void SimpleMemobj::CPUSidePort::recvFunctional(PacketPtr pkt)
+// {
+//     return owner->handleFunctional(pkt);
+// }
+
+// void SimpleMemobj::handleFunctional(PacketPtr pkt)
+// {
+//     return memPort.sendFunctional(pkt);
+// }
+
+// AddrRangeList SimpleMemobj::getAddrRanges() const
+// {
+//     DPRINTF(SimpleMemobj, "Sending new ranges\n");
+//     return memPort.getAddrRanges();
+// }
+
+// void SimpleMemobj::MemSidePort::recvRangeChange()
+// {
+//     return owner->sendRangeChange();
+// }
+
+// void SimpleMemobj::sendRangeChange()
+// {
+//     instPort.sendRangeChange();
+//     dataPort.sendRangeChange();
+// }
+
+// bool SimpleMemobj::CPUSidePort::recvTimingReq(PacketPtr pkt)
+// {
+//     if (!owner->handleRequest(pkt)) {
+//         needRetry = true;
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+// bool SimpleMemobj::handleRequest(PacketPtr pkt)
+// {
+//     if (blocked) {
+//         return false;
+//     }
+//     DPRINTF(SimpleMemobj, "Got request for address %x\n", pkt->getAddr());
+//     blocked = true;
+//     memPort.sendPacket(pkt);
+//     return true;
+// }
+
+// void SimpleMemobj::MemSidePort::sendPacket(PacketPtr pkt)
+// {
+//     panic_if(blockedPacket != nullptr, "Should never try to send if blocked!\n");
+//     if (!sendTimingReq(pkt)) {
+//         blockedPacket = pkt;
+//     }
+// }
+
+// void SimpleMemobj::MemSidePort::recvReqRetry()
+// {
+//     assert(blockedPacket != nullptr);
+
+//     PacketPtr pkt = blockedPacket;
+//     blockedPacket = nullptr;
+
+//     sendPacket(pkt);
+// }
+
+// bool SimpleMemobj::MemSidePort::recvTimingResp(PacketPtr pkt)
+// {
+//     return owner->handleResponse(pkt);
+// }
+
+// bool SimpleMemobj::handleResponse(PacketPtr pkt)
+// {
+//     assert(blocked);
+//     DPRINTF(SimpleMemobj, "Got response from address %x\n", pkt->getAddr());
+
+//     blocked = false;
+
+//     // Simply forward to the memory port
+//     if (pkt->req->isInstFetch()) {
+//         instPort.sendPacket(pkt);
+//     } else {
+//         dataPort.sendPacket(pkt);
+//     }
+
+//     instPort.trySendRetry();
+//     dataPort.trySendRetry();
+
+//     return true;
+// }
+
+// void SimpleMemobj::CPUSidePort::sendPacket(PacketPtr pkt)
+// {
+//     panic_if(blockedPacket != nullptr, "Should never try to send if blocked!\n");
+//     if (!sendTimingResp(pkt)) {
+//         blockedPacket = pkt;
+//     }
+// }
+
+// void SimpleMemobj::CPUSidePort::recvRespRetry()
+// {
+//     assert(blockedPacket != nullptr);
+
+//     PacketPtr pkt = blockedPacket;
+//     blockedPacket = nullptr;
+
+//     sendPacket(pkt);
+// }
+
+// void SimpleMemobj::CPUSidePort::trySendRetry()
+// {
+//     if (needRetry && blockedPacket == nullptr) {
+//         needRetry = false;
+//         DPRINTF(SimpleMemobj, "Sending retry for request %d\n", id);
+//         sendRetryReq();
+//     }
+// }
+
+// // SimpleMemobj* SimpleMemobjParams::create()
+// // {
+// //     return new SimpleMemobj(this);
+// // }
+
+// }
+
 #include "learning_gem5/part2/simple_memobj.hh"
 
+#include "base/trace.hh"
 #include "debug/SimpleMemobj.hh"
 
 namespace gem5
 {
 
-SimpleMemobj::SimpleMemobj(SimpleMemobjParams& params) : SimObject(params), instPort(params.name + ".inst_port", this), dataPort(params.name + ".data_port", this), memPort(params.name + ".mem_port", this), blocked(false)
+SimpleMemobj::SimpleMemobj(const SimpleMemobjParams &params) :
+    SimObject(params),
+    instPort(params.name + ".inst_port", this),
+    dataPort(params.name + ".data_port", this),
+    memPort(params.name + ".mem_side", this),
+    blocked(false)
 {
 }
 
-Port& SimpleMemobj::getPort(const std::string& if_name, PortID idx)
+Port &
+SimpleMemobj::getPort(const std::string &if_name, PortID idx)
 {
-    panic_if(idx != InvalidPortID, "This object does not support vector ports\n");
+    panic_if(idx != InvalidPortID, "This object doesn't support vector ports");
 
     // This is the name from the Python SimObject declaration (SimpleMemobj.py)
     if (if_name == "mem_side") {
@@ -21,44 +185,52 @@ Port& SimpleMemobj::getPort(const std::string& if_name, PortID idx)
     } else if (if_name == "data_port") {
         return dataPort;
     } else {
+        // pass it along to our super class
         return SimObject::getPort(if_name, idx);
     }
 }
 
-AddrRangeList SimpleMemobj::CPUSidePort::getAddrRanges() const
+void
+SimpleMemobj::CPUSidePort::sendPacket(PacketPtr pkt)
+{
+    // Note: This flow control is very simple since the memobj is blocking.
+
+    panic_if(blockedPacket != nullptr, "Should never try to send if blocked!");
+
+    // If we can't send the packet across the port, store it for later.
+    if (!sendTimingResp(pkt)) {
+        blockedPacket = pkt;
+    }
+}
+
+AddrRangeList
+SimpleMemobj::CPUSidePort::getAddrRanges() const
 {
     return owner->getAddrRanges();
 }
 
-void SimpleMemobj::CPUSidePort::recvFunctional(PacketPtr pkt)
+void
+SimpleMemobj::CPUSidePort::trySendRetry()
 {
+    if (needRetry && blockedPacket == nullptr) {
+        // Only send a retry if the port is now completely free
+        needRetry = false;
+        DPRINTF(SimpleMemobj, "Sending retry req for %d\n", id);
+        sendRetryReq();
+    }
+}
+
+void
+SimpleMemobj::CPUSidePort::recvFunctional(PacketPtr pkt)
+{
+    // Just forward to the memobj.
     return owner->handleFunctional(pkt);
 }
 
-void SimpleMemobj::handleFunctional(PacketPtr pkt)
+bool
+SimpleMemobj::CPUSidePort::recvTimingReq(PacketPtr pkt)
 {
-    return memPort.sendFunctional(pkt);
-}
-
-AddrRangeList SimpleMemobj::getAddrRanges() const
-{
-    DPRINTF(SimpleMemobj, "Sending new ranges\n");
-    return memPort.getAddrRanges();
-}
-
-void SimpleMemobj::MemSidePort::recvRangeChange()
-{
-    return owner->sendRangeChange();
-}
-
-void SimpleMemobj::sendRangeChange()
-{
-    instPort.sendRangeChange();
-    dataPort.sendRangeChange();
-}
-
-bool SimpleMemobj::CPUSidePort::recvTimingReq(PacketPtr pkt)
-{
+    // Just forward to the memobj.
     if (!owner->handleRequest(pkt)) {
         needRetry = true;
         return false;
@@ -67,45 +239,89 @@ bool SimpleMemobj::CPUSidePort::recvTimingReq(PacketPtr pkt)
     }
 }
 
-bool SimpleMemobj::handleRequest(PacketPtr pkt)
+void
+SimpleMemobj::CPUSidePort::recvRespRetry()
 {
-    if (blocked) {
-        return false;
-    }
-    DPRINTF(SimpleMemobj, "Got request for address %x\n", pkt->getAddr());
-    blocked = true;
-    memPort.sendPacket(pkt);
-    return true;
+    // We should have a blocked packet if this function is called.
+    assert(blockedPacket != nullptr);
+
+    // Grab the blocked packet.
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    // Try to resend it. It's possible that it fails again.
+    sendPacket(pkt);
 }
 
-void SimpleMemobj::MemSidePort::sendPacket(PacketPtr pkt)
+void
+SimpleMemobj::MemSidePort::sendPacket(PacketPtr pkt)
 {
-    panic_if(blockedPacket != nullptr, "Should never try to send if blocked!\n");
+    // Note: This flow control is very simple since the memobj is blocking.
+
+    panic_if(blockedPacket != nullptr, "Should never try to send if blocked!");
+
+    // If we can't send the packet across the port, store it for later.
     if (!sendTimingReq(pkt)) {
         blockedPacket = pkt;
     }
 }
 
-void SimpleMemobj::MemSidePort::recvReqRetry()
+bool
+SimpleMemobj::MemSidePort::recvTimingResp(PacketPtr pkt)
 {
-    assert(blockedPacket != nullptr);
-
-    PacketPtr pkt = blockedPacket;
-    blockedPacket = nullptr;
-
-    sendPacket(pkt);
-}
-
-bool SimpleMemobj::MemSidePort::recvTimingResp(PacketPtr pkt)
-{
+    // Just forward to the memobj.
     return owner->handleResponse(pkt);
 }
 
-bool SimpleMemobj::handleResponse(PacketPtr pkt)
+void
+SimpleMemobj::MemSidePort::recvReqRetry()
+{
+    // We should have a blocked packet if this function is called.
+    assert(blockedPacket != nullptr);
+
+    // Grab the blocked packet.
+    PacketPtr pkt = blockedPacket;
+    blockedPacket = nullptr;
+
+    // Try to resend it. It's possible that it fails again.
+    sendPacket(pkt);
+}
+
+void
+SimpleMemobj::MemSidePort::recvRangeChange()
+{
+    owner->sendRangeChange();
+}
+
+bool
+SimpleMemobj::handleRequest(PacketPtr pkt)
+{
+    if (blocked) {
+        // There is currently an outstanding request. Stall.
+        return false;
+    }
+
+    DPRINTF(SimpleMemobj, "Got request for addr %#x\n", pkt->getAddr());
+
+    // This memobj is now blocked waiting for the response to this packet.
+    blocked = true;
+
+    // Simply forward to the memory port
+    memPort.sendPacket(pkt);
+
+    return true;
+}
+
+bool
+SimpleMemobj::handleResponse(PacketPtr pkt)
 {
     assert(blocked);
-    DPRINTF(SimpleMemobj, "Got response from address %x\n", pkt->getAddr());
+    DPRINTF(SimpleMemobj, "Got response for addr %#x\n", pkt->getAddr());
 
+    // The packet is now done. We're about to put it in the port, no need for
+    // this object to continue to stall.
+    // We need to free the resource before sending the packet in case the CPU
+    // tries to send another request immediately (e.g., in the same callchain).
     blocked = false;
 
     // Simply forward to the memory port
@@ -115,42 +331,34 @@ bool SimpleMemobj::handleResponse(PacketPtr pkt)
         dataPort.sendPacket(pkt);
     }
 
+    // For each of the cpu ports, if it needs to send a retry, it should do it
+    // now since this memory object may be unblocked now.
     instPort.trySendRetry();
     dataPort.trySendRetry();
 
     return true;
 }
 
-void SimpleMemobj::CPUSidePort::sendPacket(PacketPtr pkt)
+void
+SimpleMemobj::handleFunctional(PacketPtr pkt)
 {
-    panic_if(blockedPacket != nullptr, "Should never try to send if blocked!\n");
-    if (!sendTimingResp(pkt)) {
-        blockedPacket = pkt;
-    }
+    // Just pass this on to the memory side to handle for now.
+    memPort.sendFunctional(pkt);
 }
 
-void SimpleMemobj::CPUSidePort::recvRespRetry()
+AddrRangeList
+SimpleMemobj::getAddrRanges() const
 {
-    assert(blockedPacket != nullptr);
-
-    PacketPtr pkt = blockedPacket;
-    blockedPacket = nullptr;
-
-    sendPacket(pkt);
+    DPRINTF(SimpleMemobj, "Sending new ranges\n");
+    // Just use the same ranges as whatever is on the memory side.
+    return memPort.getAddrRanges();
 }
 
-void SimpleMemobj::CPUSidePort::trySendRetry()
+void
+SimpleMemobj::sendRangeChange()
 {
-    if (needRetry && blockedPacket == nullptr) {
-        needRetry = false;
-        DPRINTF(SimpleMemobj, "Sending retry for request %d\n", id);
-        sendRetryReq();
-    }
+    instPort.sendRangeChange();
+    dataPort.sendRangeChange();
 }
 
-// SimpleMemobj* SimpleMemobjParams::create()
-// {
-//     return new SimpleMemobj(this);
-// }
-
-}
+} // namespace gem5
--- src/learning_gem5/part2/simple_memobj.hh
+++ src/learning_gem5/part2/simple_memobj.hh
@@ -1,146 +1,5 @@
-// #ifndef __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
-// #define __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
-
-// #include "mem/port.hh"
-// #include "params/SimpleMemobj.hh"
-// #include "sim/sim_object.hh"
-
-// namespace gem5
-// {
-
-// class SimpleMemobj : public SimObject
-// {
-//   private:
-//     /**
-//      * Port on the CPU-side that receives requests.
-//      * Mostly just forwards requests to the owner.
-//      * Part of a vector of ports. One for each CPU port (e.g., data, inst)
-//      */
-//     class CPUSidePort : public ResponsePort    // This class is for the ports from the CPU
-//     {
-//     private:
-//         /// The object that owns this object (SimpleMemobj)
-//         SimpleMemobj* owner;
-
-//         /// True if the port needs to send a retry req.
-//         bool needRetry;
-
-//         /// If we tried to send a packet and it was blocked, store it here
-//         PacketPtr blockedPacket;
-
-//     public:
-//         CPUSidePort(const std::string& name, SimpleMemobj* owner) : ResponsePort(name, owner), owner(owner), needRetry(false), blockedPacket(nullptr) { }
-
-//         /**
-//          * Send a packet across this port. This is called by the owner and
-//          * all of the flow control is hanled in this function.
-//          *
-//          * @param packet to send.
-//          */
-//         void sendPacket(PacketPtr pkt);
-
-//         AddrRangeList getAddrRanges() const override;
-
-//         /**
-//          * Send a retry to the peer port only if it is needed. This is called
-//          * from the SimpleMemobj whenever it is unblocked.
-//          */
-//         void trySendRetry();
-    
-//     protected:
-//         Tick recvAtomic(PacketPtr pkt) override { panic("recvAtomic unimplemented!\n"); }
-//         void recvFunctional(PacketPtr pkt) override;
-//         bool recvTimingReq(PacketPtr pkt) override;
-//         void recvRespRetry() override;
-//     };
-
-//     class MemSidePort : public RequestPort   // This class is for the ports from the memory
-//     {
-//     private:
-//         SimpleMemobj* owner;
-    
-//         /// If we tried to send a packet and it was blocked, store it here
-//         PacketPtr blockedPacket;
-//     public:
-//         MemSidePort(const std::string& name, SimpleMemobj* owner) : RequestPort(name, owner), owner(owner) { }
-
-//         /**
-//          * Send a packet across this port. This is called by the owner and
-//          * all of the flow control is hanled in this function.
-//          *
-//          * @param packet to send.
-//          */
-//         void sendPacket(PacketPtr pkt);
-
-//     protected:
-//         bool recvTimingResp(PacketPtr pkt) override;
-//         void recvReqRetry() override;
-//         void recvRangeChange() override;
-//     };
-
-
-//     /**
-//      * Handle a packet functionally. Update the data on a write and get the
-//      * data on a read.
-//      *
-//      * @param packet to functionally handle
-//      */
-//     void handleFunctional(PacketPtr pkt);
-
-//     /**
-//      * Handle the request from the CPU side
-//      *
-//      * @param requesting packet
-//      * @return true if we can handle the request this cycle, false if the
-//      *         requestor needs to retry later
-//      */
-//     bool handleRequest(PacketPtr pkt);
-
-//     /**
-//      * Handle the respone from the memory side
-//      *
-//      * @param responding packet
-//      * @return true if we can handle the response this cycle, false if the
-//      *         responder needs to retry later
-//      */
-//     bool handleResponse(PacketPtr pkt);
-
-
-//     /**
-//      * Return the address ranges this memobj is responsible for. Just use the
-//      * same as the next upper level of the hierarchy.
-//      *
-//      * @return the address ranges this memobj is responsible for
-//      */
-//     AddrRangeList getAddrRanges() const;
-
-//     /**
-//      * Tell the CPU side to ask for our memory ranges.
-//      */
-//     void sendRangeChange();
-
-//     CPUSidePort instPort;
-//     CPUSidePort dataPort;
-
-//     MemSidePort memPort;
-
-//     /// True if this is currently blocked waiting for a response.
-//     bool blocked;
-
-//   public:
-//     /** constructor
-//     */
-//     SimpleMemobj(SimpleMemobjParams& p);
-
-//     Port& getPort(const std::string& if_name, PortID idx = InvalidPortID) override;
-// };
-
-// }
-
-// #endif // __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
-
-#ifndef __LEARNING_GEM5_PART2_SIMPLE_MEMOBJ_HH__
-#define __LEARNING_GEM5_PART2_SIMPLE_MEMOBJ_HH__
+#ifndef __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
+#define __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
 
 #include "mem/port.hh"
 #include "params/SimpleMemobj.hh"
@@ -149,26 +8,19 @@
 namespace gem5
 {
 
-/**
- * A very simple memory object. Current implementation doesn't even cache
- * anything it just forwards requests and responses.
- * This memobj is fully blocking (not non-blocking). Only a single request can
- * be outstanding at a time.
- */
 class SimpleMemobj : public SimObject
 {
   private:
-
     /**
      * Port on the CPU-side that receives requests.
      * Mostly just forwards requests to the owner.
      * Part of a vector of ports. One for each CPU port (e.g., data, inst)
      */
-    class CPUSidePort : public ResponsePort
+    class CPUSidePort : public ResponsePort    // This class is for the ports from the CPU
     {
-      private:
+    private:
         /// The object that owns this object (SimpleMemobj)
-        SimpleMemobj *owner;
+        SimpleMemobj* owner;
 
         /// True if the port needs to send a retry req.
         bool needRetry;
@@ -176,14 +28,8 @@ class SimpleMemobj : public SimObject
         /// If we tried to send a packet and it was blocked, store it here
         PacketPtr blockedPacket;
 
-      public:
-        /**
-         * Constructor. Just calls the superclass constructor.
-         */
-        CPUSidePort(const std::string& name, SimpleMemobj *owner) :
-            ResponsePort(name), owner(owner), needRetry(false),
-            blockedPacket(nullptr)
-        { }
+    public:
+        CPUSidePort(const std::string& name, SimpleMemobj* owner) : ResponsePort(name, owner), owner(owner), needRetry(false), blockedPacket(nullptr) { }
 
         /**
          * Send a packet across this port. This is called by the owner and
@@ -193,13 +39,6 @@ class SimpleMemobj : public SimObject
          */
         void sendPacket(PacketPtr pkt);
 
-        /**
-         * Get a list of the non-overlapping address ranges the owner is
-         * responsible for. All response ports must override this function
-         * and return a populated list with at least one item.
-         *
-         * @return a list of ranges responded to
-         */
         AddrRangeList getAddrRanges() const override;
 
         /**
@@ -207,61 +46,23 @@ class SimpleMemobj : public SimObject
          * from the SimpleMemobj whenever it is unblocked.
          */
         void trySendRetry();
-
-      protected:
-        /**
-         * Receive an atomic request packet from the request port.
-         * No need to implement in this simple memobj.
-         */
-        Tick recvAtomic(PacketPtr pkt) override
-        { panic("recvAtomic unimpl."); }
-
-        /**
-         * Receive a functional request packet from the request port.
-         * Performs a "debug" access updating/reading the data in place.
-         *
-         * @param packet the requestor sent.
-         */
+    
+    protected:
+        Tick recvAtomic(PacketPtr pkt) override { panic("recvAtomic unimplemented!\n"); }
         void recvFunctional(PacketPtr pkt) override;
-
-        /**
-         * Receive a timing request from the request port.
-         *
-         * @param the packet that the requestor sent
-         * @return whether this object can consume the packet. If false, we
-         *         will call sendRetry() when we can try to receive this
-         *         request again.
-         */
         bool recvTimingReq(PacketPtr pkt) override;
-
-        /**
-         * Called by the request port if sendTimingResp was called on this
-         * response port (causing recvTimingResp to be called on the request
-         * port) and was unsuccesful.
-         */
         void recvRespRetry() override;
     };
 
-    /**
-     * Port on the memory-side that receives responses.
-     * Mostly just forwards requests to the owner
-     */
-    class MemSidePort : public RequestPort
+    class MemSidePort : public RequestPort   // This class is for the ports from the memory
     {
-      private:
-        /// The object that owns this object (SimpleMemobj)
-        SimpleMemobj *owner;
-
+    private:
+        SimpleMemobj* owner;
+    
         /// If we tried to send a packet and it was blocked, store it here
         PacketPtr blockedPacket;
-
-      public:
-        /**
-         * Constructor. Just calls the superclass constructor.
-         */
-        MemSidePort(const std::string& name, SimpleMemobj *owner) :
-            RequestPort(name), owner(owner), blockedPacket(nullptr)
-        { }
+    public:
+        MemSidePort(const std::string& name, SimpleMemobj* owner) : RequestPort(name, owner), owner(owner) { }
 
         /**
          * Send a packet across this port. This is called by the owner and
@@ -271,28 +72,20 @@ class SimpleMemobj : public SimObject
          */
         void sendPacket(PacketPtr pkt);
 
-      protected:
-        /**
-         * Receive a timing response from the response port.
-         */
+    protected:
         bool recvTimingResp(PacketPtr pkt) override;
-
-        /**
-         * Called by the response port if sendTimingReq was called on this
-         * request port (causing recvTimingReq to be called on the responder
-         * port) and was unsuccesful.
-         */
         void recvReqRetry() override;
-
-        /**
-         * Called to receive an address range change from the peer responder
-         * port. The default implementation ignores the change and does
-         * nothing. Override this function in a derived class if the owner
-         * needs to be aware of the address ranges, e.g. in an
-         * interconnect component like a bus.
-         */
         void recvRangeChange() override;
     };
+
+
+    /**
+     * Handle a packet functionally. Update the data on a write and get the
+     * data on a read.
+     *
+     * @param packet to functionally handle
+     */
+    void handleFunctional(PacketPtr pkt);
 
     /**
      * Handle the request from the CPU side
@@ -312,13 +105,6 @@ class SimpleMemobj : public SimObject
      */
     bool handleResponse(PacketPtr pkt);
 
-    /**
-     * Handle a packet functionally. Update the data on a write and get the
-     * data on a read.
-     *
-     * @param packet to functionally handle
-     */
-    void handleFunctional(PacketPtr pkt);
 
     /**
      * Return the address ranges this memobj is responsible for. Just use the
@@ -333,36 +119,22 @@ class SimpleMemobj : public SimObject
      */
     void sendRangeChange();
 
-    /// Instantiation of the CPU-side ports
     CPUSidePort instPort;
     CPUSidePort dataPort;
 
-    /// Instantiation of the memory-side port
     MemSidePort memPort;
 
     /// True if this is currently blocked waiting for a response.
     bool blocked;
 
   public:
-
     /** constructor
-     */
-    SimpleMemobj(const SimpleMemobjParams &params);
+    */
+    SimpleMemobj(SimpleMemobjParams& p);
 
-    /**
-     * Get a port with a given name and index. This is used at
-     * binding time and returns a reference to a protocol-agnostic
-     * port.
-     *
-     * @param if_name Port name
-     * @param idx Index in the case of a VectorPort
-     *
-     * @return A reference to the given port
-     */
-    Port &getPort(const std::string &if_name,
-                  PortID idx=InvalidPortID) override;
+    Port& getPort(const std::string& if_name, PortID idx = InvalidPortID) override;
 };
 
-} // namespace gem5
+}
 
-#endif // __LEARNING_GEM5_PART2_SIMPLE_MEMOBJ_HH__
+#endif // __LEARNING_GEM5_SIMPLE_MEMOBJ_HH__
