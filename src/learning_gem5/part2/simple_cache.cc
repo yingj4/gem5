c5d782186a20f362a33a689a863a147560977a63
yingj4 <yingj4@illinois.edu>
2024-05-10 16:07:33 -0500

    tutorial part 2 SimpleMemCache

--- src/learning_gem5/part2/simple_cache.cc
+++ src/learning_gem5/part2/simple_cache.cc
@@ -125,4 +125,196 @@ void SimpleCache::MemSidePort::recvRangeChange()
     return owner->sendRangeChange();
 }
 
+bool SimpleCache::handleRequest(PacketPtr pkt, int port_id)
+{
+    if (blocked) {
+        return false;
+    }
+    
+    DPRINTF(SimpleCache, "Got request from addr %x\n", pkt->getAddr());
+    blocked = true;
+
+    assert(waitingPortId == -1);
+    waitingPortId = port_id;
+
+    schedule(new EventFunctionWrapper([this, pkt]{ accessTiming(pkt); }, name + ".accessEvent", true)), clockEdge(latency);
+
+    return true;
+}
+
+bool SimpleCache::handleResponse(PacketPtr pkt)
+{
+    assert(blocked);
+    
+    DPRINTF(SimpleCache, "Got response for addr %x\n", pkt->getAddr());
+
+    insert(pkt);
+
+    stats.missLatency.sample(curTick() - missTime);
+
+    if (originalPacket != nullptr) {
+        DPRINTF(SimpleCache, "Copying data from a new packet to an old one\n");
+
+        [[maybe_unused]] bool hit = accessFunctional(originalPacket);
+        panic_if(!hit, "Should always hit after inserting!\n");
+        originalPacket->makeResponse();
+        delete pkt;
+        pkt = originalPacket;
+        originalPacket = nullptr;
+    }
+
+    sendResponse(pkt);
+
+    return true;
+}
+
+void SimpleCache::sendResponse(PacketPtr pkt)
+{
+    assert(blocked);
+    DPRINTF(SimpleCache, "Sending response to addr %x\n", pkt->getAddr());
+
+    int port = waitingPortId;
+
+    blocked = false;
+    waitingPortId = -1;
+
+    cpuPorts[port].sendPacket(pkt);
+
+    for (auto port : cpuPorts) {
+        port.trySendRetry();
+    }
+}
+
+void SimpleCache::handleFunctional(PacketPtr pkt)
+{
+    if (accessFunctional(pkt)) {
+        pkt->makeResponse();
+    } else {
+        memPort.sendFunctional(pkt);
+    }
+}
+
+void SimpleCache::accessTiming(PacketPtr pkt)
+{
+    bool hit = accessFunctional(pkt);
+
+    DPRINTF(SimpleCache, "%s for packet: %s\n", hit ? "Hit" : "Miss", pkt->print());
+
+    if (hit) {
+        ++stats.hits;
+        DDUMP(SimpleCache, pkt->getConstPtr<uint8_t>(), pkt->getSize());
+        pkt->makeResponse();
+        sendResponse(pkt);
+    } else {
+        ++stats.misses;
+        missTime = curTick();
+
+        Addr addr = pkt->getAddr();
+        Addr block_addr = pkt->getBlockAddr(blockSize);
+        unsigned size = pkt->getSize();
+
+        if (addr == block_addr && size == blockSize) {
+            DPRINTF(SimpleCache, "Forwarding packet\n");
+            memPort.sendPacket(pkt);
+        } else {
+            DPRINTF(SimpleCache, "Updating packet to block size\n");
+            panic_if(addr - block_addr + size > blockSize, "Cannot handle accesses that span multiple cache blocks\n");
+
+            assert(pkt->needsResponse());
+            MemCmd cmd;
+            if (pkt->isWrite() || pkt->isRead()) {
+                cmd = MemCmd::ReadReq;
+            } else {
+                panic("Unknown packet type in upgrade size\n");
+            }
+
+            PacketPtr new_pkt = new Packet(pkt->req, cmd, blockSize);
+            new_pkt->allocate();
+
+            assert(new_pkt->getAddr() == new_pkt->getBlockAddr(blockSize));
+
+            originalPacket = pkt;
+
+            DPRINTF(SimpleCache, "Forwarding packet\n");
+            memPort.sendPacket(new_pkt);
+        }
+    }
+}
+
+bool SimpleCache::accessFunctional(PacketPtr pkt)
+{
+    Addr block_addr = pkt->getBlockAddr(blockSize);
+    audo it = cacheStore.find(block_addr);
+
+    if (it != cacheStore.end()) {
+        if (pkt->isWrite()) {
+            pkt->writeDataToBlock(it->second, blockSize);
+        } else if (pkt->isRead()) {
+            pkt->setDataFromBlock(it->second, blockSize);
+        } else {
+            panic("Unknown packet type\n");
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+void SimpleCache::insert(PacketPtr pkt)
+{
+    assert(pkt->getAddr() == pkt->getBlockAddr(blockSize));
+    assert(cacheStore.find(pkt->getAddr()) == cacheStore.end());
+    assert(pkt->isResponse());
+
+    if (cacheStore.size() >= capacity) {
+        int bucket, bucket_size;
+        // Select random thing to evict. This is a little convoluted since we
+        // are using a std::unordered_map. See http://bit.ly/2hrnLP2
+        do {
+            bucket = random_mt.random(0, (int) cacheStore.bucket_count() - 1);
+        } while ((bucket_size = cacheStore.bucket_size(bucket)) == 0);
+
+        auto block = std::next(cacheStore.begin(bucket), random_mt.random(0, bucket_size - 1));
+
+        DPRINTF(SimpleCache, "Removing data at addr %x\n", block->first);
+
+        RequestPtr req = std::make_shared<Request>(block->first, blockSize, 0, 0);
+
+        PacketPtr new_pkt = new Packet(req, MemCmd::WritebackDirty, blockSize);
+        new_pkt->dataDynamic(block->second);
+
+        DPRINTF(SimpleCache, "Writing packet %s back\n", pkt->print());
+        memPort.sendPacket(new_pkt);
+        cacheStore.erase(block->first);
+    }
+
+    DPRINTF(SimpleCache, "Inserting %s\n", pkt->print());
+    DDUMP(SimpleCache, pkt->getConstPtr<uint8_t>(), blockSize);
+
+    uint8_t* data = new uint8_t[blockSize];
+    cacheStore[pkt->getAddr()] = data;
+    pkt->writeDataToBlock(data, blockSize);
+}
+
+AddrRangeList SimpleCache::getAddrRanges() const
+{
+    DPRINTF(SimpleCache, "Sending new ranges\n");
+    return memPort.getAddrRanges();
+}
+
+void SimpleCache::sendRangeChange() const
+{
+    for (auto& port : cpuPorts) {
+        port.sendRangeChange();
+    }
+}
+
+SimpleCache::SimpleCacheStats::SimpleCacheStats(statistics::Group* parent) : statistics::Group(parent), ADD_STAT(hits, statistics::units::Count()::get, "Number of hits\n"), ADD_STAT(misses, statistics::units::Count()::get, "Number of misses\n"),
+                                                                                ADD_STAT(missLatency, statistics::units::Tick::get(), "Ticks for misses to a cache\n"),
+                                                                                ADD_STAT(hitRatio, statistics::units::Ratio::get(), "The hit ratio in the cache\n", hits / (hits + misses))
+{
+    missLatency.init(16);
+}
+
 }
