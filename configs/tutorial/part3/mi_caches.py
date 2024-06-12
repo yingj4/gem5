5121dfef5b37df633881fcf1ad684ed747ffb96b
yingj4 <yingj4@illinois.edu>
2024-06-11 22:01:33 -0500

    tutorial part 3 configuring a standard protocol

--- configs/tutorial/part3/mi_caches.py
+++ configs/tutorial/part3/mi_caches.py
@@ -10,7 +10,6 @@ from m5.util import (
 class MyCacheSystem(RubySystem):
     def __init__(self):
         if buildEnv["PROTOCOL"] != "MI_example":
-            print('\n' + buildEnv["PROTOCOL"])
             fatal("This system assumes MI from learning gem5!")
         
         super().__init__()
